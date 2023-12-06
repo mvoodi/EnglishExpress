c3e4424726613f82920a872d0818bc3c3228a4f6
Aleksandra <aleksandra.arykova@alatoo.edu.kg>
2023-12-07 00:10:50 +0600

    add SpeechRecogition & random words file

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import speech_recognition as sr
+import random
+import nltk
+
+nltk.download("words")
 
 def recognize_speech():
     recognizer = sr.Recognizer()
+
+    # Добавление случайного слова перед фразой "Говорите что-то..."
+    random_word = generate_random_word()
+    print(f"Случайное слово перед началом: {random_word}")
 
     with sr.Microphone() as source:
         print("Говорите что-то...")
@@ -11,10 +19,27 @@ def recognize_speech():
         try:
             text = recognizer.recognize_google(audio, language="ru-RU")
             print(f"Вы сказали: {text}")
+
+            # Обработка различных фраз
+            handle_phrases(text)
+
         except sr.UnknownValueError:
             print("Речь не распознана")
         except sr.RequestError as e:
             print(f"Ошибка при запросе к сервису распознавания: {e}")
+
+def generate_random_word():
+    words = nltk.corpus.words.words()
+    return random.choice(words)
+
+def handle_phrases(text):
+
+    if "привет" in text.lower() or "здравствуйте" in text.lower():
+        print("Привет! Как я могу вам помочь?")
+
+
+    elif "как тебя зовут" in text.lower():
+        print("Меня зовут Боб.")
 
 if __name__ == "__main__":
     recognize_speech()
